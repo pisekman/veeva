6d066a1b168ec3ff146c8bec7388ec4393d00ada
pisekman <pisekman@gmail.com>
2022-02-19 17:58:19 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div>
+			{/* footer */}
+			{/* Start
+Clients
+Projects
+About Us
+Contacts */}
+		</div>
+	);
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            {/* WhyUS */}
+        </div>
+    )
+}
+
+export default Header
